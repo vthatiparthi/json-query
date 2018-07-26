1138032745b2f5a9d209b4e683776ad25928984e
Vikram Thatiparthi <vikky.thatiparthy@gmail.com>
2018-07-25 23:30:27 -0700

    changing pom.xml to generate fat jar

--- src/main/java/io/json/App.java
+++ src/main/java/io/json/App.java
@@ -12,7 +12,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,8 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.beust.jcommander.JCommander;
-import com.google.common.base.Functions;
-import com.google.common.collect.Ordering;
 
 public class App {
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
